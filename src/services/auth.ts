8d63e4a9608d3028e79f5d0c3a0e69e9c7ddbde5
omariado1331 <omariadocalle1331@gmail.com>
2025-12-10 14:14:48 -0400

    correcion url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //import { get } from 'react-hook-form';
 
-const API_URL = 'http://localhost:8000'; // URL API
+const API_URL = 'http://127.0.0.1:8000'; // URL API
 
 // Cuerpo del login
 interface LoginCredentials {
